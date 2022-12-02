b1060e0fb9167389b5fc2a50f40186696a0cdf58
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-12-01 23:24:00 -0300

    Update Options.hx

--- source/Options.hx
+++ source/Options.hx
@@ -1,5 +1,25 @@
 package;
 
+
+class AndroidControls extends Option
+{
+	public function new()
+	{
+		super();
+	}
+
+	public override function press():Bool
+	{
+		trace("switch");
+		FlxG.switchState(new android.AndroidControlsMenu());
+		return false;
+	}
+
+	private override function updateDisplay():String
+	{
+		return "Android Controls";
+	}
+}
 class Options
 {
 	public static var masterVolume:Float = 1;
